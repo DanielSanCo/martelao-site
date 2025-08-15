4a6528d61dab112a9ad55458cb02311de1b9995f
Daniel <96082772+DanielSanCo@users.noreply.github.com>
2025-08-15 11:21:20 -0300

    version 3

--- util/produtos.ts
+++ util/produtos.ts
@@ -1,140 +1,200 @@
 const Produtos = [
     {
-        nome: 'Lampada Led 9w bulbo luz Branca Fria', 
-        codigo: '25497', 
-        img: '/img/lampadaLedAsus.jfif', 
+        nome: 'Lampada Led 9w bulbo luz Branca Fria',
+        codigo: '25497',
+        img: '/img/lampadaLedAsus.jfif',
         preco: 3.75,
-        desc: '', 
+        desc: '',
         categoria: 'Eletrica'
     },
     {
-        nome: 'conversor gravador digital de tv cd700 intelbras', 
-        codigo: '22318', 
-        img: '/img/gravadorConversorIntelbras.png', 
+        nome: 'conversor gravador digital de tv cd700 intelbras',
+        codigo: '22318',
+        img: '/img/gravadorConversorIntelbras.png',
         preco: 100,
-        desc: '', 
+        desc: '',
         categoria: 'Eletrica'
     },
     {
-        nome: 'Cadeira Madeira Dobravel Imbuia', 
-        codigo: '13875', 
-        img: '/img/cadeiraDobravel.png', 
+        nome: 'Cadeira Madeira Dobravel Imbuia',
+        codigo: '13875',
+        img: '/img/cadeiraDobravel.png',
         preco: 107.5,
-        desc: '', 
+        desc: '',
         categoria: 'Eletrica'
     },
     {
-        nome: 'Mesa Madeira Dobravel Quadrada Imbuia 70x70', 
-        codigo: '13876', 
-        img: '/img/mesaDobravel.png', 
+        nome: 'Mesa Madeira Dobravel Quadrada Imbuia 70x70',
+        codigo: '13876',
+        img: '/img/mesaDobravel.png',
         preco: 265,
-        desc: '', 
+        desc: '',
         categoria: 'Utencilios'
     },
     {
-        nome: 'Tinta Acrilica Coralar Amarelo Canario Lata 18lt', 
-        codigo: '08278', 
-        img: '/img/tintaCoralar.png', 
+        nome: 'Tinta Acrilica Coralar Amarelo Canario Lata 18lt',
+        codigo: '08278',
+        img: '/img/tintaCoralar.png',
         preco: 255,
-        desc: '', 
+        desc: '',
         categoria: 'Tinta',
         opcoes: [
-            {nome: 'Amarelo Canario', preco: 255, codigo: '08278', cor: '#f8d588'},
-            {nome: 'Amarelo Fervo', preco: 255, codigo: '23025', cor: ''},
-            {nome: 'Areia', preco: 255, codigo: '08267', cor: ''},
-            {nome: 'Azul Pavão', preco: 261.24, codigo: '17783', cor: ''},
-            {nome: 'Camurça', preco: 255, codigo: '08296', cor: ''},
-            {nome: 'Concreto', preco: 255, codigo: '17159', cor: ''},
-            {nome: 'Laranja Maracatu', preco: 265.63, codigo: '23028', cor: ''},
+            { nome: 'Amarelo Canario', preco: 255, codigo: '08278', cor: '#f8d588' },
+            { nome: 'Amarelo Fervo', preco: 255, codigo: '23025', cor: '' },
+            { nome: 'Areia', preco: 255, codigo: '08267', cor: '' },
+            { nome: 'Azul Pavão', preco: 261.24, codigo: '17783', cor: '' },
+            { nome: 'Camurça', preco: 255, codigo: '08296', cor: '' },
+            { nome: 'Concreto', preco: 255, codigo: '17159', cor: '' },
+            { nome: 'Laranja Maracatu', preco: 265.63, codigo: '23028', cor: '' },
 
-            
-            {nome: 'Lua do Sertão', preco: 261.24, codigo: '08278', cor: ''},
-            {nome: 'Marfim', preco: 263.75, codigo: '08262', cor: ''},
-            {nome: 'Oceano', preco: 265.63, codigo: '08283', cor: ''},
-            {nome: 'Palha', preco: 255.63, codigo: '08266', cor: ''},
-            {nome: 'Perola', preco: 255, codigo: '08259', cor: ''},
 
-            
-            {nome: 'Pessego', preco: 270, codigo: '08286', cor: ''},
-            {nome: 'Rubi', preco: 290.63, codigo: '25648', cor: ''},
-            {nome: 'Verde Limão', preco: 255, codigo: '08282', cor: ''},
-            {nome: 'Verde Timbalada', preco: 255, codigo: '23568', cor: ''},
-            {nome: 'Verde Vale', preco: 255, codigo: '08275', cor: ''},
+            { nome: 'Lua do Sertão', preco: 261.24, codigo: '08278', cor: '' },
+            { nome: 'Marfim', preco: 263.75, codigo: '08262', cor: '' },
+            { nome: 'Oceano', preco: 265.63, codigo: '08283', cor: '' },
+            { nome: 'Palha', preco: 255.63, codigo: '08266', cor: '' },
+            { nome: 'Perola', preco: 255, codigo: '08259', cor: '' },
+
+
+            { nome: 'Pessego', preco: 270, codigo: '08286', cor: '' },
+            { nome: 'Rubi', preco: 290.63, codigo: '25648', cor: '' },
+            { nome: 'Verde Limão', preco: 255, codigo: '08282', cor: '' },
+            { nome: 'Verde Timbalada', preco: 255, codigo: '23568', cor: '' },
+            { nome: 'Verde Vale', preco: 255, codigo: '08275', cor: '' },
         ]
     },
     {
-        nome: 'Tinta Profissional Coral *Branca Lata 18lt', 
-        codigo: '25100', 
-        img: '/img/tintaProCoral.png', 
+        nome: 'Tinta Profissional Coral *Branca Lata 18lt',
+        codigo: '25100',
+        img: '/img/tintaProCoral.png',
         preco: 216.25,
-        desc: '', 
+        desc: '',
         categoria: 'Tinta'
     },
     {
-        nome: 'Telha Colonial 2.30x0.86 PVC cor terracota', 
-        codigo: '18344', 
-        img: '/img/telhaColonial.jpg', 
+        nome: 'Telha Colonial 2.30x0.86 PVC cor terracota',
+        codigo: '18344',
+        img: '/img/telhaColonial.jpg',
         preco: 123.75,
-        desc: '', 
+        desc: '',
         categoria: 'Eletrica'
     },
     {
-        nome: 'Telha PVC leitosa 2.44x1.10 *Afort', 
-        codigo: '07668', 
-        img: '/img/telhaLeitosa.png', 
+        nome: 'Telha PVC leitosa 2.44x1.10 *Afort',
+        codigo: '07668',
+        img: '/img/telhaLeitosa.png',
         preco: 171.24,
-        desc: '', 
+        desc: '',
         categoria: 'Telha',
         opcoes: [
-            {nome: '2.44x1.10', preco: 171.24, codigo: '07668'},
-            {nome: '2.44x0.50', preco: 84.37, codigo: '07669'}
+            { nome: '2.44x1.10', preco: 171.24, codigo: '07668' },
+            { nome: '2.44x0.50', preco: 84.37, codigo: '07669' }
         ]
     },
     {
-        nome: 'Telha Fibro Incolor 2.44x1.10 Afort', 
-        codigo: '13190', 
-        img: '/img/telhaIncolor.png', 
+        nome: 'Telha Fibro Incolor 2.44x1.10 Afort',
+        codigo: '13190',
+        img: '/img/telhaIncolor.png',
         preco: 108.73,
-        desc: '', 
+        desc: '',
         categoria: 'Telha',
         opcoes: [
-            {nome: '2.44x1.10', preco: 108.73, codigo: '13190'},
-            {nome: '2.44x0.50', preco: 84.37, codigo: '13191'}
+            { nome: '2.44x1.10', preco: 108.73, codigo: '13190' },
+            { nome: '2.44x0.50', preco: 84.37, codigo: '13191' }
         ]
     },
     {
-        nome: 'Telha Fibrocimento 2.44x1.10 5mm', 
-        codigo: '19447', 
-        img: '/img/telhaFibrocimento.png', 
+        nome: 'Telha Fibrocimento 2.44x1.10 5mm',
+        codigo: '19447',
+        img: '/img/telhaFibrocimento.png',
         preco: 76.87,
-        desc: '', 
+        desc: '',
         categoria: 'Telha'
     },
     {
-        nome: 'Telha Plan 2.42x0.88 PVC cor marfim Precon', 
-        codigo: '12680', 
-        img: '/img/telhaPlan.png', 
+        nome: 'Telha Plan 2.42x0.88 PVC cor marfim Precon',
+        codigo: '12680',
+        img: '/img/telhaPlan.png',
         preco: 127.48,
-        desc: '', 
+        desc: '',
         categoria: 'Telha'
     },
     {
-        nome: 'Conduite Tigreflex 20mm 50mt - Tigre', 
-        codigo: '00317', 
-        img: '/img/conduiteTigreAmarelo.png', 
+        nome: 'Conduite Tigreflex 20mm 50mt - Tigre',
+        codigo: '00317',
+        img: '/img/conduiteTigreAmarelo.png',
         preco: 155.62,
-        desc: '', 
+        desc: '',
         categoria: 'Conduite'
     },
     {
-        nome: 'Conduite Amarelo Reforçado 25mm 50mt - Fortlev', 
-        codigo: '03462', 
-        img: '/img/conduiteAmareloFortlev.png', 
+        nome: 'Conduite Amarelo Reforçado 25mm 50mt - Fortlev',
+        codigo: '03462',
+        img: '/img/conduiteAmareloFortlev.png',
         preco: 71.24,
-        desc: '', 
+        desc: '',
         categoria: 'Conduite'
     },
-    
+    /* -------------------------------------------------------- */
+    {
+        nome: "TINTA CHEGA DE MOFO 3.6L CORAL",
+        codigo: '15281',
+        preco: 181.87,
+        img: ""
+    },
+    {
+        nome: "TINTA ESMALTE HEMERITE BR 800ML CORAL",
+        codigo: '06255',
+        preco: 99.99,
+        img: ""
+    },
+    {
+        nome: "TINTA ESM BASE DAGUA BR ACETINA 1/4 CORAL",
+        codigo: '11726',
+        preco: 58.12,
+        img: ""
+    },
+    {
+        nome: "TINTA ESM CORALIT BRILHO AZUL FRANCA 900ML CORAL",
+        codigo: '07779',
+        preco: 57.48,
+        img: ""
+    },
+    {
+        nome: "VENTILADOR PAREDE PRETO 60CM BIVOLT VEN",
+        codigo: '01780',
+        preco: 351.24,
+        img: ""
+    },
+    {
+        nome: "TRILHO 200CM BRANCO",
+        codigo: '07451',
+        preco: 64.37,
+        img: ""
+    },
+    {
+        nome: "TELHA ISOPOR 0.97X1.99 - MARTELAO",
+        codigo: '27233',
+        preco: 62.48,
+        img: ""
+    },
+    {
+        nome: "PRIMER BASE SOLVENTE GL 3.6L DRYKO",
+        codigo: '20184',
+        preco: 87.48,
+        img: "/img/primer3.6L.png"
+    },
+    {
+        nome: "PRIMER BASE AGUA 1LT DRYKO",
+        codigo: '20757',
+        preco: 22.48,
+        img: ""
+    },
+    {
+        nome: "CONTRA UMIDADE SACO 500ML VIAPOL UN",
+        codigo: '25081',
+        preco: 6.23,
+        img: ""
+    }
 ]
 
 export default Produtos;
